499a1a989948a27b5e18d185a778a4807945b5f4
IrinaPeshko <privet-kira1999@mail.ru>
2024-01-26 17:37:20 +0300

    feat: create copy, create and rename functions in fs module

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import * as fs from "node:fs";
+import url from "node:url";
+import path from "path";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const fileMessage = "I am fresh and young";
+  const errorMessage = "FS operation failed";
+  
+  fs.writeFile(
+    path.join(__dirname, "files", "fresh.txt"),
+    fileMessage,
+    {
+      flag: "wx",
+    },
+    (err) => {
+      if (err) {
+        throw new Error(errorMessage);
+      }
+    }
+  );
 };
 
 await create();
