490a5aad5555ed1f645b516fa5f26e16f30d974d
IrinaPeshko <privet-kira1999@mail.ru>
2024-01-29 13:14:37 +0300

    feat: create Zlib module

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createGunzip } from "node:zlib";
+import fs from "fs";
+import path from "path";
+import url from "node:url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const sourcePath = path.join(__dirname, "files", "archive.gz");
+  const destinationPath = path.join(__dirname, "files", "fileToCompress.txt");
+  const gzip = createGunzip();
+  const sourceStream = fs.createReadStream(sourcePath);
+  const destinationStream = fs.createWriteStream(destinationPath);
+
+  sourceStream.pipe(gzip).pipe(destinationStream);
 };
 
 await decompress();
